9c556abdf564f7b7a37ae59767a03805616eb87b
FlaviiaTorres <flaviiadev@gmail.com>
2024-06-13 14:59:07 -0300

    iniciando o projeto

---
+++ PlataformaReduque/src/main/java/git/flavia/PlataformaReduque/PlataformaReduqueApplication.java
@@ -0,0 +1,13 @@
+package git.flavia.PlataformaReduque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaReduqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaReduqueApplication.class, args);
+	}
+
+}
---
+++ PlataformaReduque/src/test/java/git/flavia/PlataformaReduque/PlataformaReduqueApplicationTests.java
@@ -0,0 +1,13 @@
+package git.flavia.PlataformaReduque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaReduqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
