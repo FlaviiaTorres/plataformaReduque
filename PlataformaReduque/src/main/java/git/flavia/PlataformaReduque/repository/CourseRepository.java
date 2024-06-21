0e2da34943ca4a50dac79fa8c89957d80555c734
FlaviiaTorres <flaviiadev@gmail.com>
2024-06-21 11:06:47 -0300

    ajustando as claases de servico

--- PlataformaReduque/src/main/java/git/flavia/PlataformaReduque/repository/CourseRepository.java
+++ PlataformaReduque/src/main/java/git/flavia/PlataformaReduque/repository/CourseRepository.java
@@ -1,9 +1,13 @@
 package git.flavia.PlataformaReduque.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import git.flavia.PlataformaReduque.model.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long>{
+	
+	 List<Course> findByInstructorId(Long instructorId);
 
 }
